4052e289e88cbc25df805e4e5caaae8dbbb0c254
gustavowl <gustavoalvesbezerra@gmail.com>
2017-11-24 13:48:38 -0300

    Adds declaration of some auxiliary functions

--- metaheur-gravity/repair_operator.h
+++ metaheur-gravity/repair_operator.h
@@ -3,12 +3,17 @@
 
 //arg1: solution vector for which the repair operator will be applied
 //arg2: the transpose of the adjacency matrix
-void repair_operator(float **solution_vector, int **transp_adj_matrix);
+void repair_operator(float **solution_vector, int **transp_adj_matrix, int num_vertices);
 
 //used internally
-void rpr_oprtr_add_phase(float **solution_vector, int **transp_adj_matrix);
+void rpr_oprtr_add_phase(float **solution_vector, int **transp_adj_matrix, int num_vertices);
 
 //used internally
-void rpr_oprtr_drop_phase(float **solution_vector, int **transp_adj_matrix);
+void rpr_oprtr_drop_phase(float **solution_vector, int **transp_adj_matrix, int num_vertices);
+
+//used internally
+void rpr_oprtr_cost_of_column(int col, int **transp_adj_matrix, int num_vertices);
+
+float** rpr_oprtr_multiply(float **vector, int **transp_adj_matrix, int num_vertices);
 
 #endif
