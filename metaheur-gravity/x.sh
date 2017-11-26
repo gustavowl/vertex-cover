eb48a4a60c955cd3ac2af28dbaa678a5b9634e4a
gustavowl <gustavoalvesbezerra@gmail.com>
2017-11-25 21:00:43 -0300

    Adds script for 50 vertices instances

---
+++ metaheur-gravity/x.sh
@@ -0,0 +1,298 @@
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.1 >> g1.txt
+
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+./gravity ../graph50/graph50-01.txt 50 1000 1 0.9 >> g2.txt
+
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.1 >> g3.txt
+
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+./gravity ../graph50/graph50-06.txt 50 1000 1 0.9 >> g4.txt
+
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.1 >> g5.txt
+
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+./gravity ../graph50/graph50-09.txt 50 1000 1 0.9 >> g6.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.1 >> g17.txt
+
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 150 1000 1 0.9 >> g18.txt
+
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.1 >> g7.txt
+
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 150 1000 1 0.9 >> g8.txt
+
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.1 >> g9.txt
+
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 150 1000 1 0.9 >> g10.txt
+
+
+
+
+
+
+
+
+
+
+
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.1 >> g17.txt
+
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+./gravity ../graph50/graph50-01.txt 50 1000 10000 0.9 >> g18.txt
+
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.1 >> g7.txt
+
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+./gravity ../graph50/graph50-06.txt 50 1000 10000 0.9 >> g8.txt
+
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.1 >> g9.txt
+
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+./gravity ../graph50/graph50-09.txt 50 1000 10000 0.9 >> g10.txt
+
+
+
+
+
+
+
+
+
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.1 >> g11.txt
+
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+./gravity ../graph50/graph50-01.txt 50 10000 10000 0.9 >> g12.txt
+
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.1 >> g13.txt
+
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+./gravity ../graph50/graph50-06.txt 50 10000 10000 0.9 >> g14.txt
+
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.1 >> g15.txt
+
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
+./gravity ../graph50/graph50-09.txt 50 10000 10000 0.9 >> g16.txt
