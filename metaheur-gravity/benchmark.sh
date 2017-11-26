b6b5fd62b2749288e99e73aab5a5f0bd6f9f2021
gustavowl <gustavoalvesbezerra@gmail.com>
2017-11-25 22:34:23 -0300

    Updates benchmark

--- metaheur-gravity/benchmark.sh
+++ metaheur-gravity/benchmark.sh
@@ -4,30 +4,29 @@
 FILE_EXTENSION=".txt"
 MAX_ITERATIONS=5
 GRAPH=(graph100 frb30-15)
-VERTICES=(100 450)
+INIT_SOLUTIONS=(10 25 50 100 200 450)
 INSTANCES=(01 03 05)
 NUM_ITERATIONS=10000
 TIME_UNVRS=(1 10000)
 BETA_VALUES=(0.1 0.9)
 
 count_script=0
-max_script=$[${#LANGUAGES[@]} * $MAX_ITERATIONS * ${#MATRIX_DIM[@]}]
-index=0
+max_script=$[${#INIT_SOLUTIONS[@]} * ${#GRAPH[@]} * ${#TIME_UNVRS[@]} * ${#BETA_VALUES[@]} * $MAX_ITERATIONS]
 
-for i in ${GRAPH[@]};
+for init_sols in ${INIT_SOLUTIONS[@]};
 do
-	for j in "${INSTANCES[@]}";
+	for i in ${GRAPH[@]};
 	do
-		for time in "${TIME_UNVRS[@]}";
+		for j in "${INSTANCES[@]}";
 		do
-			for beta in "${BETA_VALUES[@]}";
+			for time in "${TIME_UNVRS[@]}";
 			do
-				for vert in `seq 1 $[$index+1]`;
+				for beta in "${BETA_VALUES[@]}";
 				do
 					mkdir -p $i"/shell/" #guarantees that path exists
 					mkdir -p $i"/internal/" #guarantees that path exists
-					FILE_SHELL_PREFIX=$i"/shell/"$i"-"$j
-					FILE_STD_OUTPUT_PREFIX=$i"/internal/"$i"-"$j
+					FILE_SHELL_PREFIX=$i"/shell/"$i"-"$j"-"$init_sols"-"$time"-"$beta
+					FILE_STD_OUTPUT_PREFIX=$i"/internal/"$i"-"$j"-"$init_sols"-"$time"-"$beta
 					count_iterations=0
 					#runs the program the desired number of times
 					echo -e '\n\n'--------------------BEGIN $i-$j--------------------
@@ -39,11 +38,9 @@ do
 						rm -f $FILE_STD_OUTPUT #so it doesnt print unnecessary info
 						#choose the matrices randomly since the order for square matrices does not matter
 						ARGUMENT1="../"$i"/"$i"-"$j$FILE_EXTENSION
-						ARGUMENT2=${VERTICES[$vert]}
-						ARGUMENT3=$time
 
 						start=$( date -I'ns' )
-						"./gravity" $ARGUMENT1 $ARGUMENT2 $NUM_ITERATIONS $time $beta >> $FILE_STD_OUTPUT
+						"./gravity" $ARGUMENT1 $init_sols $NUM_ITERATIONS $time $beta >> $FILE_STD_OUTPUT
 						end=$( date -I'ns' )
 						echo "" >> $FILE_STD_OUTPUT
 						echo "-------------------------ITERATION #"$k"-------------------------" >> $FILE_SHELL
@@ -56,12 +53,10 @@ do
 						count_iterations=$[$count_iterations + 1]
 						perc_script=$(bc -l <<< 'scale=2; '$count_script*100/$max_script)
 						#perc_iterations=$(bc -l <<< 'scale=1; '$count_iterations*100/$MAX_ITERATIONS)
-						echo -e $i $j $[$count_iterations*100/$MAX_ITERATIONS]% '\t'"Script progress: "$count_script/$max_script '('$perc_script%')'
+						echo -e $i"-"$j"-"$k $[$count_iterations*100/$MAX_ITERATIONS]% '\t'"Script progress: "$count_script/$max_script '('$perc_script%')'
 					done
 				done
 			done
 		done
 	done
-
-	index=$[$index + 1]
 done
